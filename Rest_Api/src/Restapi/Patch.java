fc992b1d93532ed0ec929f1a824f80f041d63679
Admin <Admin@DESKTOP-JKSER83>
2022-12-27 15:42:55 +0530

    Build102

---
+++ Rest_Api/src/Restapi/Patch.java
@@ -0,0 +1,30 @@
+package Restapi;
+
+import org.json.simple.JSONObject;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Patch 
+{
+	public static void main(String[] args) 
+	{
+		RestAssured.baseURI="https://reqres.in/";
+		RequestSpecification r = RestAssured.given();
+		
+		
+		JSONObject obj=new JSONObject();
+		obj.put("name","morpheus");
+		obj.put("job","zion resident");
+		r.contentType(ContentType.JSON);
+		r.body(obj.toJSONString());
+		
+		Response res = r.request(Method.PATCH,"/api/users/2");
+		System.out.println(res.asString());
+		int code = res.statusCode();
+		System.out.println(code);
+}
+}
